9613494c9a024a425a539baeb79a5186bf0b16c7
kfinc <karolinafinc@gmail.com>
2019-04-15 20:07:55 +0200

    Added denoising interface

--- fmridenoise/interfaces/denoising.py
+++ fmridenoise/interfaces/denoising.py
@@ -5,48 +5,36 @@ from nipype.utils.filemanip import split_filename
 import nibabel as nb
 import numpy as np
 import os
+import pandas as pd
 
-class BasicDenoisingInterfaceInputSpec(BaseInterfaceInputSpec):
-    confounds = File(exists=True, desc='confounds file', mandatory=True)
-    strategy = File(exists=True, desc='denoising strategy to apply', mandatory=True)
-    # threshold = traits.Float(desc='everything below this value will be set to zero',
-    #                          mandatory=True)
+class DenoiseInputSpec(BaseInterfaceInputSpec):
+    fmri_preprocessed = File(exists=True, desc='preprocessed fMRI file', mandatory=True)
+    confounds = File(exists=True, desc='confound file', mandatory=True)
 
+class DenoiseOutputSpec(TraitedSpec):
+    fmri_denoised = File(exists=True, desc='denoised fMRI file', mandatory=True)
 
-class BasicdenoisingInterfaceOutputSpec(TraitedSpec):
-    ...
-    #thresholded_volume = File(exists=True, desc="thresholded volume")
-
-
-class SimpleThreshold(BaseInterface):
-    input_spec = BasicDenoisingInterfaceInputSpec
-    output_spec = BasicdenoisingInterfaceOutputSpec
+class Denoise(BaseInterface):
+    input_spec = DenoiseInputSpec
+    output_spec = DenoiseOutputSpec
 
     def _run_interface(self, runtime):
-        """
-        Calls denoising function with appropriate strategy.
-        Musi ręcznie zapisać pliki, które mają być potem używane do daleszej analizy.
-        :param runtime:
-        :return:
-        """
-        # fname = self.inputs.volume
-        # img = nb.load(fname)
-        # data = np.array(img.get_data())
-        #
-        # active_map = data > self.inputs.threshold
-        #
-        # thresholded_map = np.zeros(data.shape)
-        # thresholded_map[active_map] = data[active_map]
-        #
-        # new_img = nb.Nifti1Image(thresholded_map, img.affine, img.header)
-        # _, base, _ = split_filename(fname)
-        # nb.save(new_img, base + '_thresholded.nii')
+        fname = self.inputs.fmri_preprocessed
+        img = nb.load(fname)
+        cname = self.inputs.confounds
+        conf = pd.read_csv(cname, delimiter = '\t')
+        conf = conf.iloc[:,:3].values
+
+        denoised_img = clean_img(img, confounds=conf)
+
+        _, base, _ = split_filename(fname)
+        nb.save(denoised_img, base + '_denoised.nii')
 
         return runtime
 
-    # def _list_outputs(self):
-    #     outputs = self._outputs().get()
-    #     fname = self.inputs.volume
-    #     _, base, _ = split_filename(fname)
-    #     outputs["thresholded_volume"] = os.path.abspath(base + '_thresholded.nii')
-    #     return outputs
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        fname = self.inputs.fmri_preprocessed
+        _, base, _ = split_filename(fname)
+        #outputs["denoised_fmri"] = os.path.abspath(base + '_denoised.nii')
+        return outputs
