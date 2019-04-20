c8bc3bdf364cd047f1e2535fccf66a3c45149dc3
kfinc <karolinafinc@gmail.com>
2019-04-20 14:05:40 +0200

    Updated workflow sketch

--- fmridenoise/workflows/base.py
+++ fmridenoise/workflows/base.py
@@ -2,51 +2,31 @@ from nipype.pipeline import engine as pe
 from ..interfaces.loading_bids import (
     PipelineSpecLoader, LoadPipeline, BIDSSelect, BIDSDataSink)
 from ..interfaces.denoising import Denoising
+from ..interfaces.loading_bids import BIDSSelect
 
-def init_fmridenoise_wf(bids_dir, derivatives, out_dir, pipelines_dir, desc=None,
-                        ignore=None, force_index=None,
-                        model=None, participants=None,
-                        base_dir=None, name='fmridenoise_wf'):
-    wf = pe.Workflow(name=name, base_dir=base_dir)
+def init_fmridenoise_wf(bids_dir, derivatives, entities,
+                        #out_dir, 
+                        #pipelines_dir, desc=None,
+                        #ignore=None, force_index=None,
+                        #model=None, participants=None,
+                        base_dir=None, name='fmridenoise_wf'
+                       ):
+    
+    wf = pe.Workflow(name='fmridenoise', base_dir=None)
 
-    # Find the appropriate pipeline file(s)
-    specs = PipelineSpecLoader(pipelines_dir=pipelines_dir) #--> create this interface
-    #if model is not None:
-    #    specs.inputs.model = model
+    loading_bids = pe.Node(
+                BIDSSelect(
+                    bids_dir=bids_dir, 
+                    derivatives=True, 
+                    entities = entities),
+                name='loading_bids')
+                # Outputs: bold_files, confounds_files, entities 
 
-    pipeline_dict = specs.run().outputs.pipeline_spec #--> create this interface
-
-    if not pipeline_dict:
-        raise RuntimeError("Unable to find pipeline specification file")
-    #if isinstance(model_dict, list):
-    #    raise RuntimeError("Currently unable to run multiple models in parallel - "
-    #                       "please specify model")
-
-
-    loader = pe.Node(
-            LoadBIDSModel(bids_dir=bids_dir,
-                          derivatives=derivatives,
-                          pipeline=pipeline_dict),
-            name='loader')
+    denoising = pe.Node(
+            Denoise(),
+            name='denoising')
 
 
-    # Select preprocessed BOLD series to analyze
-    getter = pe.Node(
-        BIDSSelect(
-            bids_dir=bids_dir, derivatives=derivatives,
-            selectors={
-                'suffix': 'bold',
-                'desc': desc),
-            name='getter')
-
-    denoising = pe.MapNode(
-        Denoising(),
-        iterfield=['session_info', 'bold_file', 'confounds'],
-        name='denoising')
-
-
-    # General connections
-    wf.connect([
-        (loader,
-        getter,
-        denoising)
+        # General connections
+    wf.connect([loading_bids, denoising, [('bold','bold_files'), 
+                                          ('confound', 'confound_files')]])
