ab36afcad23adc306d73acec12d5b36325b653b5
SiegfriedWagner <mateus.chojnowski@gmail.com>
2019-04-23 17:31:10 +0200

    wip

--- fmridenoise/workflows/base.py
+++ fmridenoise/workflows/base.py
@@ -4,10 +4,10 @@ import fmridenoise
 import os
 import glob
 from nipype import Node, Workflow
-from ..interfaces.bids import BIDSSelect
+from ..interfaces.loading_bids import BIDSSelect, BIDSLoad
 
 
-def init_fmridenoise_wf(bids_dir, derivatives, entities,
+def init_fmridenoise_wf(bids_dir, derivatives,
                         # out_dir,
                         pipelines_paths = glob.glob(os.path.dirname(fmridenoise.__file__) + "/pipelines/*"),
                         #, desc=None,
@@ -21,13 +21,17 @@ def init_fmridenoise_wf(bids_dir, derivatives, entities,
     pipelineselector = Node(PipelineSelector(), name="PipelineSelector")
     pipelineselector.iterables = ('pipeline_path', pipelines_paths)
     # Outputs: dictionary with pipeline
-
-    loading_bids = Node(
-        BIDSSelect(
-            bids_dir=bids_dir,
-            derivatives=True,
-            entities=entities),
-        name='loading_bids')
+    # Inputs: directory
+    loading_bids = Node(BIDSLoad(), name="BidsLoader")
+    loading_bids.inputs.bids_dir = bids_dir
+    loading_bids.inputs.derivatives = True
+    # Outputs: entities
+    selecting_bids = Node(
+        BIDSSelect(),
+        name='BidsSelector')
+    selecting_bids.inputs.bids_dir = bids_dir
+    selecting_bids.inputs.derivatives = True
+    selecting_bids.inputs.selectors = None
     # Outputs: bold_files, confounds_files, entities
 
     denoising = Node(
@@ -35,8 +39,7 @@ def init_fmridenoise_wf(bids_dir, derivatives, entities,
         name='denoising')
 
     # General connections
-    wf.connect([loading_bids, denoising, [('bold', 'bold_files'),
-                                          ('confound', 'confound_files')]])
+    wf.connect([loading_bids, selecting_bids, ('entities', 'entities')])
 
 #prepconfounds = Node()
 
