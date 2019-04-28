7c480ac265a31660427495d2863bff4726287054
kfinc <karolinafinc@gmail.com>
2019-04-28 16:43:28 +0200

    BaseInterface to SimpleInterface

--- fmridenoise/interfaces/confounds.py
+++ fmridenoise/interfaces/confounds.py
@@ -8,8 +8,10 @@ from fmridenoise.utils import confound_prep
 import pandas as pd
 import os
 from nipype.interfaces.base import BaseInterface, \
-    BaseInterfaceInputSpec, traits, File, TraitedSpec
+    BaseInterfaceInputSpec, traits, File, TraitedSpec, SimpleInterface
 from nipype.utils.filemanip import split_filename
+from confound_prep import prep_conf_df
+
 
 class ConfoundsInputSpec(BaseInterfaceInputSpec):
     pipeline = traits.Dict(
@@ -20,49 +22,75 @@ class ConfoundsInputSpec(BaseInterfaceInputSpec):
         desc='confounds table',
         mandatory=True)
 
+
 class ConfoundsOutputSpec(TraitedSpec):
     conf_prep = File(
         exists=True,
         desc="preprocessed confounds table")
 
-class Confounds(BaseInterface):
+
+class Confounds(SimpleInterface):
     input_spec = ConfoundsInputSpec
     output_spec = ConfoundsOutputSpec
 
     def _run_interface(self, runtime):
 
         fname = self.inputs.conf_raw
+
         conf_df_raw = pd.read_csv(fname, sep='\t')
 
-        # Preprocess confound table acording to pipeline
+        # Preprocess confound table according to pipeline
         conf_df_prep = prep_conf_df(conf_df_raw, self.inputs.pipeline)
 
         # Create new filename and save
         path, base, _ = split_filename(fname)
         fname_prep = f"{path}/{base}_{self.inputs.pipeline['name']}_prep.tsv"
         conf_df_prep.to_csv(fname_prep, sep='\t', index=False)
+        self._results['conf_prep'] = fname_prep
 
         return runtime
 
-    def _list_outputs(self):
+    # def _list_outputs(self):
+    #
+    #     outputs = self._outputs().get()
+    #     fname = self.inputs.conf_raw
+    #     path, base, _ = split_filename(fname)
+    #     fname_prep = f"{path}/{base}_{self.inputs.pipeline['name']}_prep.tsv"
+    #     self.outputs["conf_prep"] = fname_prep
+    #
+    #     return outputs
 
-        outputs = self._outputs().get()
-        fname = self.inputs.conf_raw
-        path, base, _ = split_filename(fname)
-        fname_prep = f"{path}/{base}_{self.inputs.pipeline['name']}_prep.tsv"
-        outputs["conf_prep"] = fname_prep
+# if __name__ == "__main__":
+#     import utils as ut
+#
+#     # jdicto = ut.load_pipeline_from_json("/home/kmb/Desktop/Neuroscience/" + \
+#     #    "Projects/confound_removal/nbraingroup/fmridenoise/pipelines/36_parameters_spikes.json")
+#
+#     jdicto = ut.load_pipeline_from_json("../pipelines/36_parameters_spikes.json")
+#     confpath = "/home/finc/Dropbox/Projects/fitlins/BIDS/derivatives/fmriprep/sub-09/func/" + \
+#                "sub-09_task-rhymejudgment_desc-confounds_regressors.tsv"
+#
+#     cf = Confounds()
+#     cf.inputs.pipeline = jdicto
+#     cf.inputs.conf_raw = confpath
+#     cf.run()
+#
+#     print(cf._results['conf_prep'])
 
-        return outputs
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from nipype import Node
     import utils as ut
 
-    jdicto = ut.load_pipeline_from_json("/home/kmb/Desktop/Neuroscience/" + \
-        "Projects/confound_removal/nbraingroup/fmridenoise/pipelines/36_parameters_spikes.json")
-    confpath = "/home/kmb/Desktop/Neuroscience/" + \
-        "Projects/confound_removal/nbraingroup/fmridenoise/pipelines/sub-kb01_task-prlrew_desc-confounds_regressors.tsv"
+    prep_conf = Node(Confounds(), name="ConfPrep")
+
+    jdicto = ut.load_pipeline_from_json("../pipelines/36_parameters_spikes.json")
+    confpath = "/home/finc/Dropbox/Projects/fitlins/BIDS/derivatives/fmriprep/sub-09/func/" + \
+               "sub-09_task-rhymejudgment_desc-confounds_regressors.tsv"
 
     cf = Confounds()
     cf.inputs.pipeline = jdicto
     cf.inputs.conf_raw = confpath
-    cf.run()
+    results = cf.run()
+
+    print(results.outputs)
