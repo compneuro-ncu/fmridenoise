38dbed8c71228a2e3cb882a547aa4b8c852e69e1
kfinc <karolinafinc@gmail.com>
2019-04-29 14:43:13 +0200

    Added output_dir

--- fmridenoise/interfaces/confounds.py
+++ fmridenoise/interfaces/confounds.py
@@ -15,19 +15,20 @@ from confound_prep import prep_conf_df
 
 class ConfoundsInputSpec(BaseInterfaceInputSpec):
     pipeline = traits.Dict(
-        desc='denoising pipeline',
+        desc='Denoising pipeline',
         mandatory=True)
     conf_raw = File(
         exist=True,
-        desc='confounds table',
+        desc='Confounds table',
         mandatory=True)
+    output_dir = File(          # needed to save data in other directory
+        desc='Output path')
 
 
 class ConfoundsOutputSpec(TraitedSpec):
     conf_prep = File(
-    #    exists=True,
-    #    desc="preprocessed confounds table"
-    )
+        exists=True,
+        desc="Preprocessed confounds table")
 
 
 class Confounds(SimpleInterface):
@@ -37,19 +38,22 @@ class Confounds(SimpleInterface):
     def _run_interface(self, runtime):
 
         fname = self.inputs.conf_raw
-
         conf_df_raw = pd.read_csv(fname, sep='\t')
 
         # Preprocess confound table according to pipeline
         conf_df_prep = prep_conf_df(conf_df_raw, self.inputs.pipeline)
 
         # Create new filename and save
-        path, base, _ = split_filename(fname)
-        fname_prep = f"{path}/{base}_{self.inputs.pipeline['name']}_prep.tsv"
+        path, base, _ = split_filename(fname)  # Path can be removed later
+        fname_prep = f"{self.inputs.output_dir}/{base}_{self.inputs.pipeline['name']}_prep.tsv"  # use output path
         conf_df_prep.to_csv(fname_prep, sep='\t', index=False)
         self._results['conf_prep'] = fname_prep
 
         return runtime
+
+
+
+# --- TESTS
 
 if __name__ == '__main__':
     from nipype import Node
@@ -64,6 +68,7 @@ if __name__ == '__main__':
     cf = Confounds()
     cf.inputs.pipeline = jdicto
     cf.inputs.conf_raw = confpath
+    cf.inputs.output_dir = '/home/finc/Dropbox/Projects/fitlins/BIDS/derivatives/fmridenoise'
     results = cf.run()
 
     print(results.outputs)
