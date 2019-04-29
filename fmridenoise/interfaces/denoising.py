83662bcd363e7a0540ea1a80cfe559efddcb6da5
kfinc <karolinafinc@gmail.com>
2019-04-29 15:18:35 +0200

    Minor edits

--- fmridenoise/interfaces/denoising.py
+++ fmridenoise/interfaces/denoising.py
@@ -5,31 +5,41 @@ from nipype.interfaces.base import (
     )
 
 import nibabel as nb
-import numpy as np
 import os
 import pandas as pd
+from nilearn.image import clean_img
+
 
 class DenoiseInputSpec(BaseInterfaceInputSpec):
-    fmri_preprocessed = File(exists=True, desc='preprocessed fMRI file', mandatory=True)
-    confounds = File(exists=True, desc='confound file', mandatory=True)
+    fmri_prep = File(exists=True,
+                     desc='Preprocessed fMRI file',
+                     mandatory=True)
+    conf_prep = File(exists=True,
+                     desc='Confound file',
+                     mandatory=True)
+    output_dir = File(desc='Output path')
+
 
 class DenoiseOutputSpec(TraitedSpec):
-    fmri_denoised = File(exists=True, desc='denoised fMRI file', mandatory=True)
+    fmri_denoised = File(exists=True,
+                         desc='Denoised fMRI file',
+                         mandatory=True)
+
 
 class Denoise(SimpleInterface):
     input_spec = DenoiseInputSpec
     output_spec = DenoiseOutputSpec
 
     def _run_interface(self, runtime):
-        fname = self.inputs.fmri_preprocessed
+        fname = self.inputs.fmri_prep
         img = nb.load(fname)
-        cname = self.inputs.confounds
-        conf = pd.read_csv(cname, delimiter = '\t')
+        cname = self.inputs.conf_prep
+        conf = pd.read_csv(cname, delimiter='\t')
 
         denoised_img = clean_img(img, confounds=conf)
 
         _, base, _ = split_filename(fname)
-        nb.save(denoised_img, base + '_denoised.nii')
+        nb.save(denoised_img, f'{self.inputs.output_dir}/{base}_denoised.nii')
         self._results['fmri_denoised'] = os.path.abspath(base + '_denoised.nii')
 
         return runtime
