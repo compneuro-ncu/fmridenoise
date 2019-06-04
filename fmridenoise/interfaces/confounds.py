d9dc501ba6068c34c943900a233662e6591b107b
kfinc <karolinafinc@gmail.com>
2019-06-04 17:15:18 +0200

    Aggregating dictionaries

--- fmridenoise/interfaces/confounds.py
+++ fmridenoise/interfaces/confounds.py
@@ -56,23 +56,67 @@ class Confounds(SimpleInterface):
         return runtime
 
 
+
+class AggConfoundsInputSpec(BaseInterfaceInputSpec):
+    conf_summary = traits.List(
+        exists=True,
+        desc="Confounds summary")
+    output_dir = File(          # needed to save data in other directory
+        desc="Output path")     # TODO: Implement temp dir
+
+
+class AggConfoundsOutputSpec(TraitedSpec):
+    group_conf_summary = File(
+        exists=True,
+        desc="Confounds summary")
+
+class AggConfounds(SimpleInterface):
+    input_spec = AggConfoundsInputSpec
+    output_spec = AggConfoundsOutputSpec
+
+    def _run_interface(self, runtime):
+        group_conf_summary = pd.DataFrame()
+
+        for summary in self.inputs.conf_summary:
+            group_conf_summary = group_conf_summary.append(pd.DataFrame.from_dict(summary))
+            fname = f"{self.inputs.output_dir}group_confounds_table.tsv"
+            group_conf_summary.to_csv(fname, sep='\t', index=False)
+            self._results['group_conf_summary'] = fname
+            return runtime
+
+
 # --- TESTS
+
+# if __name__ == '__main__':
+#     from nipype import Node
+#     import utils as ut
+#
+#     prep_conf = Node(Confounds(), name="ConfPrep")
+#
+#     #jdicto = ut.load_pipeline_from_json("../pipelines/36_parameters_spikes.json")
+#     jdicto = ut.load_pipeline_from_json("../pipelines/pipeline-acomp_cor.json")
+#     confpath = "/media/finc/Elements/BIDS_pseudowords/BIDS/derivatives/fmriprep/sub-01/func/" + \
+#                "sub-01_task-rhymejudgment_desc-confounds_regressors.tsv"
+#
+#     conf_summaries = [{'max_fd': [0.25027780000000005], 'mean_fd': [0.11377746541635224], 'n_conf': [35], 'n_spikes': [8]}, {'max_fd': [0.3917644499999999], 'mean_fd': [0.14487157071698104], 'n_conf': [58], 'n_spikes': [31]}]
+#
+#     cf = Confounds()
+#     cf.inputs.pipeline = jdicto
+#     cf.inputs.conf_raw = confpath
+#     cf.inputs.output_dir = '/media/finc/Elements/fmridenoise/derivatives/fmridenoise/'
+#     results = cf.run()
+#
+#     print(results.outputs)
 
 if __name__ == '__main__':
     from nipype import Node
     import utils as ut
 
-    prep_conf = Node(Confounds(), name="ConfPrep")
 
-    #jdicto = ut.load_pipeline_from_json("../pipelines/36_parameters_spikes.json")
-    jdicto = ut.load_pipeline_from_json("../pipelines/pipeline-acomp_cor.json")
-    confpath = "/media/finc/Elements/BIDS_pseudowords/BIDS/derivatives/fmriprep/sub-01/func/" + \
-               "sub-01_task-rhymejudgment_desc-confounds_regressors.tsv"
+    conf_summaries = [{'max_fd': [0.25027780000000005], 'mean_fd': [0.11377746541635224], 'n_conf': [35], 'n_spikes': [8]}, {'max_fd': [0.3917644499999999], 'mean_fd': [0.14487157071698104], 'n_conf': [58], 'n_spikes': [31]}]
 
-    cf = Confounds()
-    cf.inputs.pipeline = jdicto
-    cf.inputs.conf_raw = confpath
+    cf = AggConfounds()
     cf.inputs.output_dir = '/media/finc/Elements/fmridenoise/derivatives/fmridenoise/'
+    cf.inputs.conf_summary = conf_summaries
     results = cf.run()
-
     print(results.outputs)
