3dc4a331bf428c37a45fc8cd3c14b4f2f5956161
kfinc <karolinafinc@gmail.com>
2019-08-16 16:18:21 +0200

    Add comments to quality measures interface

--- fmridenoise/interfaces/quality_measures.py
+++ fmridenoise/interfaces/quality_measures.py
@@ -30,6 +30,7 @@ class QualityMeasuresInputSpec(BaseInterfaceInputSpec):
                            mandatory=True)
 
     output_dir = File(desc='Output path')
+
     pipeline_name = traits.Str(mandatory=True)
 
 
@@ -44,7 +45,8 @@ class QualityMeasuresOutputSpec(TraitedSpec):
 
     edges_weight_clean = traits.Dict(
         exists=True,
-        desc="Weights of individual edges")
+        desc="Weights of individual edges after "
+             "removing subjects with high motion")
 
 
 class QualityMeasures(SimpleInterface):
@@ -53,16 +55,17 @@ class QualityMeasures(SimpleInterface):
 
     def _run_interface(self, runtime):
         # Loading data
-        group_corr_mat = np.load(self.inputs.group_corr_mat)
-        group_conf_summary = pd.read_csv(self.inputs.group_conf_summary, sep='\t')
+        group_corr_mat = np.load(self.inputs.group_corr_mat)  # array with matrices for all runs
+        group_conf_summary = pd.read_csv(self.inputs.group_conf_summary, sep='\t')  # motion summary for all runs
         pipeline_name = self.inputs.pipeline_name
-        distance_vector = sym_matrix_to_vec(np.load(self.inputs.distance_matrix))
+        distance_vector = sym_matrix_to_vec(np.load(self.inputs.distance_matrix))  # load distance matrix
 
         # Creating vectors with subject filter
         all_sub_no = len(group_conf_summary)
         icluded_sub = group_conf_summary["include"]
-        excluded_sub_no = all_sub_no - sum(icluded_sub)
+        excluded_sub_no = all_sub_no - sum(icluded_sub) # number of subjects excluded from analyses
 
+        # Create dictionary describing full sampple and sample after exluding highly motion runs
         included = {f"All subjects (n = {all_sub_no})":
                         [np.ones((all_sub_no), dtype=bool), False, all_sub_no, "All"],
                     f"After excluding {excluded_sub_no} high motion subjects (n = {all_sub_no - excluded_sub_no})":
@@ -82,13 +85,15 @@ class QualityMeasures(SimpleInterface):
 
             for i in range(n_edges):
                 corr = pearsonr(group_corr_vec[value[0], i], group_conf_summary['mean_fd'].values[value[0]])
-                fc_fd_corr[i] = corr[0]
-                fc_fd_pval[i] = corr[1]
+                fc_fd_corr[i] = corr[0]  # Pearson's r values
+                fc_fd_pval[i] = corr[1]  # p-values
 
             fc_fd_corr = np.nan_to_num(fc_fd_corr)  # TODO: write exception
 
+            # Calculate correlation between FC-FD r values and distance vector
             distance_dependence = pearsonr(fc_fd_corr, distance_vector)[0]
 
+            # Store summary measure
             fc_fd_summary.append({"pipeline": pipeline_name,
                                   "perc_fc_fd_uncorr": np.sum(fc_fd_pval < 0.5) / len(fc_fd_pval) * 100,
                                   "pearson_fc_fd": np.median(fc_fd_corr),
@@ -98,15 +103,15 @@ class QualityMeasures(SimpleInterface):
                                   "subjects": value[3],
                                   "sub_no": value[2]
                                   })
-
+            # For cleaned dataset
             if value[1]:
                 edges_weight_clean = {pipeline_name: group_corr_vec[value[0]].mean(axis=0)}
 
+            # For full dataset
             if not value[1]:
                 edges_weight = {pipeline_name: group_corr_vec[value[0]].mean(axis=0)}
 
-
-            # --- plotting matrices
+            # Plotting FC and FC-FD correlation matrices
             vec = vec_to_sym_matrix(fc_fd_corr)
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
 
@@ -157,24 +162,28 @@ class PipelinesQualityMeasuresInputSpec(BaseInterfaceInputSpec):
     fc_fd_summary = traits.List(
         exists=True,
         desc="QC-FC quality measures")
+
     edges_weight = traits.List(
         exists=True,
         desc="Weights of individual edges")
+
     edges_weight_clean = traits.List(
         exists=True,
         desc="Weights of individual edges")
+
     output_dir = File(          # needed to save data in other directory
         desc="Output path")     # TODO: Implement temp dir
-
 
 class PipelinesQualityMeasuresOutputSpec(TraitedSpec):
 
     pipelines_fc_fd_summary = File(
         exists=True,
         desc="Group QC-FC quality measures")
+
     pipelines_edges_weight = File(
         exists=True,
         desc="Group weights of individual edges")
+
     pipelines_edges_weight_clean = File(
         exists=True,
         desc="Group weights of individual edges")
@@ -185,7 +194,10 @@ class PipelinesQualityMeasures(SimpleInterface):
 
     def _run_interface(self, runtime):
 
-        pipelines_fc_fd_summary = pd.DataFrame(list(chain.from_iterable(list(chain.from_iterable(self.inputs.fc_fd_summary)))))
+        # Convert merged quality measures to pd.DataFrame
+        pipelines_fc_fd_summary = pd.DataFrame(
+            list(chain.from_iterable(list(chain.from_iterable(self.inputs.fc_fd_summary)))))
+
         pipelines_edges_weight = pd.DataFrame()
         pipelines_edges_weight_clean = pd.DataFrame()
 
@@ -203,9 +215,11 @@ class PipelinesQualityMeasures(SimpleInterface):
         pipelines_edges_weight.to_csv(fname2, sep='\t', index=False)
         pipelines_edges_weight_clean.to_csv(fname3, sep='\t', index=False)
 
-        # --- Plotting
+        # ----------------------
+        # Plot quality measures
+        # ----------------------
 
-        # density plot (all subjects)
+        # Density plot (all subjects)
         fig1, ax = plt.subplots(1, 1)
 
         for col in pipelines_edges_weight:
@@ -215,7 +229,7 @@ class PipelinesQualityMeasures(SimpleInterface):
 
         fig1.savefig(f"{self.inputs.output_dir}/pipelines_edges_density.svg", dpi=300)
 
-        # density plot (no high motion)
+        # Density plot (no high motion)
         fig1_2, ax = plt.subplots(1, 1)
 
         for col in pipelines_edges_weight_clean:
@@ -225,7 +239,7 @@ class PipelinesQualityMeasures(SimpleInterface):
 
         fig1_2.savefig(f"{self.inputs.output_dir}/pipelines_edges_density_no_high_motion.svg", dpi=300)
 
-        # boxplot (Pearson's r FC-DC)
+        # Boxplot (Pearson's r FC-DC)
         fig2 = sns.catplot(x="pearson_fc_fd",
                     y="pipeline",
                     col='subjects',
@@ -235,7 +249,7 @@ class PipelinesQualityMeasures(SimpleInterface):
                                     ylabel='Pipeline')
         fig2.savefig(f"{self.inputs.output_dir}/pipelines_fc_fd_pearson.svg", dpi=300, bbox_inches="tight")
 
-        # boxplot (% correlated edges)
+        # Boxplot (% correlated edges)
         fig3 = sns.catplot(x="perc_fc_fd_uncorr",
                     y="pipeline",
                     col='subjects',
@@ -246,7 +260,7 @@ class PipelinesQualityMeasures(SimpleInterface):
 
         fig3.savefig(f"{self.inputs.output_dir}/pipelines_fc_fd_uncorr.svg", dpi=300, bbox_inches="tight")
 
-        # boxplot (Pearson's r FC-DC with distance)
+        # Boxplot (Pearson's r FC-DC with distance)
 
         fig4 = sns.catplot(x="distance_dependence",
                     y="pipeline",
@@ -257,26 +271,8 @@ class PipelinesQualityMeasures(SimpleInterface):
                                     ylabel='Pipeline')
         fig4.savefig(f"{self.inputs.output_dir}/pipelines_distance_dependence.svg", dpi=300, bbox_inches="tight")
 
-
-
         self._results['pipelines_fc_fd_summary'] = fname1
         self._results['pipelines_edges_weight'] = fname2
         self._results['pipelines_edges_weight_clean'] = fname3
 
         return runtime
-
-
-
-# --- TESTING
-
-if __name__ == '__main__':
-
-    qc = QualityMeasures()
-
-    qc.inputs.group_conf_summary = '/media/finc/Elements/BIDS_pseudowords/BIDS/derivatives/fmridenoise/36_parameters_spikes_group_conf_summary.tsv'
-    qc.inputs.group_corr_mat = '/media/finc/Elements/BIDS_pseudowords/BIDS/derivatives/fmridenoise/36_parameters_spikes_group_corr_mat.npy'
-    qc.inputs.output_dir = "/home/finc/"
-    qc.inputs.pipeline_name = "test"
-
-    results = qc.run()
-    print(results.outputs)
