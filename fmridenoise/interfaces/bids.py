e0d31c541ea4f7e4bc5f9659036c9263be7a8758
kbonna <kongokou@gmail.com>
2019-09-16 00:19:33 +0200

    fixed and improved BIDSGrab interface

--- fmridenoise/interfaces/bids.py
+++ fmridenoise/interfaces/bids.py
@@ -1,5 +1,5 @@
 # Interface for loading preprocessed fMRI data and confounds table
-
+from bids import BIDSLayout
 from nipype.interfaces.io import IOBase
 from nipype.utils.filemanip import split_filename, copyfile
 from nipype.interfaces.base import (
@@ -7,7 +7,105 @@ from nipype.interfaces.base import (
     traits, isdefined, TraitedSpec,
     Directory, File, Str, ImageFile,
     InputMultiObject, OutputMultiObject, OutputMultiPath, InputMultiPath)
+import json
 import os
+
+
+def validate_derivatives(bids_dir, derivatives):
+    """ Validate derivatives argument provided by the user.
+
+    Args:
+        bids_dir: list
+            Path to bids root directory.
+        derivatives: str or list(str)
+            Derivatives to use for denoising.
+
+    Returns:
+        derivatives_valid: list
+            Validated derivatives list.
+        scope: list
+            Right scope keyword used in pybids query.
+    """
+
+    if isinstance(derivatives, str):
+        derivatives_valid = [derivatives]
+    else:
+        derivatives_valid = derivatives
+
+    # Create full paths to derivatives folders
+    derivatives_valid = [os.path.join(bids_dir, 'derivatives', d)
+                    for d in derivatives_valid]
+
+    # Establish right scope keyword for arbitrary packages
+    scope = []
+    for derivative_path in derivatives_valid:
+        dataset_desc_path = os.path.join(derivative_path,
+                                         'dataset_description.json')
+        try:
+            with open(dataset_desc_path, 'r') as f:
+                dataset_desc = json.load(f)
+            scope.append(dataset_desc['PipelineDescription']['Name'])
+        except FileNotFoundError as e:
+            raise Exception(f"{derivative_path} should contain" +
+                            " dataset_description.json file") from e
+        except KeyError as e:
+            raise Exception(f"Key 'PipelineDescription.Name' is " +
+                            "required in {dataset_desc_path} file") from e
+
+    return derivatives_valid, scope
+
+
+def validate_option(layout, option, kind='task'):
+    """ Validate BIDS query filters provided by the user.
+
+    Args:
+        layout: bids.layout.layout.BIDSLayout
+            Lightweight class representing BIDS project file tree.
+        option: list
+            Filter arguments provided by the user.
+        kind: string
+            Type of query. Available options are 'task', 'session' and
+            'subject'.
+
+    Returns:
+        option_: list
+            Validated filter values.
+    """
+    # Grab all possible filter values
+
+    if kind == 'task':
+        option_all = layout.get_tasks()
+    elif kind == 'session':
+        option_all = layout.get_sessions()
+    elif kind == 'subject':
+        option_all = layout.get_subjects()
+    else:
+        raise ValueError("kind should be either 'task', 'session' or 'subject'")
+
+    option_ = option
+    for option_item in option_:
+        if option_item not in option_all:
+            raise ValueError(f'{kind} {option_item} is not found')
+
+    return option_
+
+
+def compare_common_entities(file1, file2) -> None:
+    """Compare common entities for two layout files"""
+
+    common_keys = ['task', 'session', 'subject', 'datatype']
+    entity_f1 = {key: value for key, value in file1.get_entities().items() if
+                 key in common_keys}
+    entity_f2 = {key: value for key, value in file2.get_entities().items() if
+                 key in common_keys}
+
+    if not entity_f1 == entity_f2:
+        raise MissingFile(f"{file1.path} has no corresponding file. "
+                          f"Entities {entity_f1} and {entity_f2} should match.")
+
+
+class MissingFile(IOError):
+    pass
 
 
 class BIDSGrabInputSpec(BaseInterfaceInputSpec):
@@ -47,7 +145,7 @@ class BIDSGrabOutputSpec(TraitedSpec):
     fmri_prep = OutputMultiPath(ImageFile)
     fmri_prep_aroma = OutputMultiPath(ImageFile)
     conf_raw = OutputMultiPath(File)
-    conf_json = OutputMultiPath(File)  # TODO: Kamil check
+    conf_json = OutputMultiPath(File)
     entities = OutputMultiObject(traits.Dict)
     tr_dict = traits.Dict()
 
@@ -91,83 +189,6 @@ class BIDSGrab(SimpleInterface):  # TODO: update documentation
 
     def _run_interface(self, runtime):
 
-        import json
-        from bids import BIDSLayout
-
-        def validate_derivatives(bids_dir, derivatives):
-            """ Validate derivatives argument provided by the user.
-
-            Args:
-                bids_dir: list
-                    Path to bids root directory.
-                derivatives: str or list(str)
-                    Derivatives to use for denoising.
-
-            Returns:
-                derivatives_: list
-                    Validated derivatives list.
-                scope: list
-                    Right scope keyword used in pybids query.
-            """
-
-            if isinstance(derivatives, str):
-                derivatives_ = [derivatives]
-            else:
-                derivatives_ = derivatives
-
-            # Create full paths to derivatives folders
-            derivatives_ = [os.path.join(bids_dir, 'derivatives', d)
-                            for d in derivatives_]
-
-            # Establish right scope keyword for arbitrary packages
-            scope = []
-            for derivative_path in derivatives_:
-                dataset_desc_path = os.path.join(derivative_path,
-                                                 'dataset_description.json')
-                try:
-                    with open(dataset_desc_path, 'r') as f:
-                        dataset_desc = json.load(f)
-                    scope.append(dataset_desc['PipelineDescription']['Name'])
-                except FileNotFoundError as e:
-                    raise Exception(f"{derivative_path} should contain" +
-                                    " dataset_description.json file") from e
-                except KeyError as e:
-                    raise Exception(f"Key 'PipelineDescription.Name' is " +
-                                    "required in {dataset_desc_path} file") from e
-
-            return derivatives_, scope
-
-        def validate_option(layout, option, kind='task'):
-            """ Validate BIDS query filters provided by the user.
-
-            Args:
-                layout: bids.layout.layout.BIDSLayout
-                    Lightweight class representing BIDS project file tree.
-                option: list
-                    Filter arguments provided by the user.
-                kind: string
-                    Type of query. Available options are 'task', 'session' and
-                    'subject'.
-
-            Returns:
-                option_: list
-                    Validated filter values.
-            """
-            # Grab all possible filter values
-            if kind == 'task':
-                option_all = layout.get_tasks()
-            elif kind == 'session':
-                option_all = layout.get_sessions()
-            elif kind == 'subject':
-                option_all = layout.get_subjects()
-
-            option_ = option
-            for option_item in option_:
-                if option_item not in option_all:
-                    raise ValueError(f'{kind} {option_item} is not found')
-
-            return option_
-
         # Validate derivatives argument
         derivatives, scope = validate_derivatives(
             bids_dir=self.inputs.bids_dir,
@@ -176,8 +197,9 @@ class BIDSGrab(SimpleInterface):  # TODO: update documentation
 
         layout = BIDSLayout(
             root=self.inputs.bids_dir,
+            derivatives=derivatives,
             validate=True,
-            derivatives=derivatives
+            index_metadata=False
         )
 
         # Validate optional arguments
@@ -197,8 +219,6 @@ class BIDSGrab(SimpleInterface):  # TODO: update documentation
             filter_base['subject'] = subject
 
         # Define query filters
-        keys_entities = ['task', 'session', 'subject', 'datatype']
-
         filter_fmri = {
             'extension': ['nii', 'nii.gz'],
             'suffix': 'bold',
@@ -213,121 +233,62 @@ class BIDSGrab(SimpleInterface):  # TODO: update documentation
             'extension': 'tsv',
             'suffix': 'regressors',
             'desc': 'confounds',
-        }  # for later
+        }
         filter_conf_json = {
             'extension': 'json',
             'suffix': 'regressors',
             'desc': 'confounds',
         }
-
         filter_fmri.update(filter_base)
+        filter_fmri_aroma.update(filter_base)
+        filter_conf.update(filter_base)
+        filter_conf_json.update(filter_base)
 
-        ########################################################################
-        ### SOLUTION FOR LATER #################################################
-        ########################################################################
-        # filter_fmri_aroma.update(filter_base)
-        # filter_conf.update(filter_base)
-        # filter_conf_json.update(filter_base)
+        # Grab all requested files
+        fmri_prep = layout.get(scope=scope, **filter_fmri)
+        if self.inputs.ica_aroma:
+            fmri_prep_aroma = layout.get(scope=scope, **filter_fmri_aroma)
+        conf_raw = layout.get(scope=scope, **filter_conf)
+        conf_json = layout.get(scope=scope, **filter_conf_json)
 
-        # # Grab all requested files
-        # fmri_prep = layout.get(scope=scope, **filter_fmri)
-        # if self.inputs.ica_aroma:
-        #     fmri_prep_aroma = layout.get(scope=scope, **filter_fmri_aroma)
-        # conf_raw = layout.get(scope=scope, **filter_conf)
-        # conf_json = layout.get(scope=scope, **filter_conf_json)
-        ########################################################################
-        ########################################################################
-        ########################################################################
+        # Validate correspondence between queried files
+        entities = []
+        for i, fmri_file in enumerate(fmri_prep):
 
-        fmri_prep, fmri_prep_aroma, conf_raw, conf_json, entities = ([] for _ in
-                                                                     range(5))
-
-        for fmri_file in layout.get(scope=scope, **filter_fmri):
-
-            entity_bold = fmri_file.get_entities()
-
-            # Look for corresponding confounds file
-            filter_entities = {key: value
-                               for key, value in entity_bold.items()
-                               if key in keys_entities}
-
-            # Constraining search
-            filter_conf.update(filter_entities)
-            filter_conf_json.update(filter_entities)
-
-            conf_file = layout.get(scope=scope, **filter_conf)
-            conf_json_file = layout.get(scope=scope, **filter_conf_json)
-
-            if not conf_file:
-                raise FileNotFoundError(
-                    f"Regressor file not found for file {fmri_file.path}"
-                )
-            else:
-                # Add entity only if both files are available
-                if len(conf_file) > 1:
-                    print(
-                        f"Warning: Multiple regressors found for file {fmri_file.path}.\n"
-                        f"Selecting {conf_file[0].path}"
-                    )  # TODO: find proper warning (logging?)
-
-                conf_file = conf_file[0]
-
-            if not conf_json_file:
-                raise FileNotFoundError(
-                    f"Regressor file not found for file {fmri_file.path}"
-                )
-            else:
-                # Add entity only if both files are available
-                if len(conf_json_file) > 1:
-                    print(
-                        f"Warning: Multiple .json regressors found for file {fmri_file.path}.\n"
-                        f"Selecting {conf_json_file[0].path}"
-                    )
-
-                conf_json_file = conf_json_file[0]
-
+            # reference common entities for preprocessed files
             if self.inputs.ica_aroma:
-                filter_fmri_aroma.update(
-                    filter_entities)  # Add specific fields to constrain search
-                fmri_aroma_file = layout.get(scope=scope, **filter_fmri_aroma)
+                compare_common_entities(fmri_file, fmri_prep_aroma[i])
+            compare_common_entities(fmri_file, conf_raw[i])
+            compare_common_entities(fmri_file, conf_json[i])
 
-                if not fmri_aroma_file:
-                    raise FileNotFoundError(
-                        f"ICA-Aroma file not found for file {fmri_file.path}"
-                    )
-
-                else:
-                    # Add entity only if both files are available
-                    if len(fmri_aroma_file) > 1:
-                        print(
-                            f"Warning: Multiple ICA-Aroma files found for file {fmri_file.path}.\n"
-                            f"Selecting {fmri_aroma_file[0].path}"
-                        )
-                    # TODO: find proper warning (logging?)
-
-                    fmri_aroma_file = fmri_aroma_file[0]
-                    fmri_prep_aroma.append(fmri_aroma_file.path)
-
-            fmri_prep.append(fmri_file.path)
-            conf_raw.append(conf_file.path)
-            conf_json.append(conf_json_file.path)
-            entities.append(filter_entities)
+            entities.append({key: value for key, value in
+                             fmri_file.get_entities().items()
+                             if key in ['task', 'session', 'subject', 'datatype']})
 
         # Extract TRs
         tr_dict = {}
+
+        #TODO: this is just a funny workaround, look for better solution later
+        layout_for_tr = BIDSLayout(
+            root=self.inputs.bids_dir,
+            derivatives=derivatives,
+            validate=True,
+            index_metadata=True
+        )
 
         for t in task:
             filter_fmri_tr = filter_fmri.copy()
             filter_fmri_tr['task'] = t
 
-            example_file = layout.get(**filter_fmri_tr)[0]
-            tr = layout.get_metadata(example_file.path)['RepetitionTime']
-            tr_dict[t] = tr
+            example_file = layout_for_tr.get(**filter_fmri_tr)[0]
+            tr_dict[t] = layout_for_tr.get_metadata(example_file.path)['RepetitionTime']
+            # tr_dict[t] = 2
 
-        self._results['fmri_prep'] = fmri_prep
-        self._results['fmri_prep_aroma'] = fmri_prep_aroma
-        self._results['conf_raw'] = conf_raw
-        self._results['conf_json'] = conf_json
+        self._results['fmri_prep'] = [file.path for file in fmri_prep]
+        if self.inputs.ica_aroma:
+            self._results['fmri_prep_aroma'] = [file.path for file in fmri_prep_aroma]
+        self._results['conf_raw'] = [file.path for file in conf_raw]
+        self._results['conf_json'] = [file.path for file in conf_json]
         self._results['entities'] = entities
         self._results['tr_dict'] = tr_dict
 
@@ -378,18 +339,13 @@ class BIDSDataSink(IOBase):
 
 
 # --- TESTS
-
 if __name__ == '__main__':
 
-    bids_dir = '/media/finc/Elements/fMRIDenoise_data/BIDS_ds000003-00001/'
-    #task = ['rest']
-    #session = ['1']
-    ica_aroma = True
+    bids_dir = "./../../tests/interfaces/bids_grab/decidenet_fmriprep_dummy"
+    task = ['prlrew']
+    subject = ['m03', 'm04']
 
-    grabber = BIDSGrab(
-        bids_dir=bids_dir,
-        ica_aroma=ica_aroma
-    )
-
+    grabber = BIDSGrab(bids_dir=bids_dir)
     result = grabber.run()
+
     print(result.outputs)
