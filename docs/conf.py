edcc3e00f30d42bfd1bab63a533e6715a6907861
Tenteed <51666227+Tenteed@users.noreply.github.com>
2020-05-18 20:00:03 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'recommonmark'
+extensions = ['sphinx.ext.autodoc', 'm2r',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,7 +56,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['docs']
-
-from recommonmark.parser import CommonMarkParser
 
 source_suffix = ['.rst', '.md']
