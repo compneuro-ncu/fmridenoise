ddc120935916ef97912186fffe0c2a24a8f6542f
Tenteed <51666227+Tenteed@users.noreply.github.com>
2020-05-18 21:05:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,8 +33,8 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import m2r
-extensions = ['sphinx.ext.autodoc', 'm2r',
+autodoc_mock_imports = ['m2r']
+extensions = ['sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
