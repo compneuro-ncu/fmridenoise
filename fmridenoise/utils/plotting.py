32ff11187dfbec4ba23f5eb0bbfff6f57746a196
kfinc <karolinafinc@gmail.com>
2020-05-28 23:02:05 +0200

    Corrected motion_plot

--- fmridenoise/utils/plotting.py
+++ fmridenoise/utils/plotting.py
@@ -1,13 +1,17 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from os.path import join
 
-def motion_plot(group_conf_summary):
-    # Plot style setup
+
+def make_motion_plot(group_conf_summary, pipeline_name, output_dir,
+                     mean_fd_th=0.2, max_fd_th=5, perc_spikes_th=20):
+    """Generates plot presenting number of subjects excluded with high morion
+    according specified thresholds."""
+
     plt.style.use('seaborn-white')
-    plt.rcParams['font.family'] = 'Helvetica'
-    colour = ["#fe6863", "#00a074"]
-    palette = sns.set_palette(colour)
+    colors = ['#00a074', '#fe6863']
+    palette = sns.set_palette(colors, 2)
 
     small = 15
     plt.rc('font', size=small)  # controls default text sizes
@@ -18,42 +22,41 @@ def motion_plot(group_conf_summary):
     plt.rc('ytick', labelsize=small)  # fontsize of the tick labels
     plt.rc('legend', fontsize=small)  # legend fontsize
     plt.rc('lines', linewidth=2.2, color='gray')
-    # ------------------------------------------
 
-    motion_dict = {'Mean FD': ['mean_fd', 0.2],
-                   'Max FD': ['max_fd', 5],
-                   'Percent of outlier dataframes (%)': ['perc_spikes', 20]}
+    columns = ['mean_fd', 'max_fd', 'perc_spikes']
+    thresholds = [mean_fd_th, max_fd_th, perc_spikes_th]
 
     fig, axes = plt.subplots(1, 3, figsize=(16, 7))
     fig.subplots_adjust(wspace=0.4, hspace=0.4)
 
-    i = 0
-    for key, value in motion_dict.items():
-        plt.figure(figsize=(4, 6))
-        p = sns.swarmplot(y=value[0],
+    for i, (column, threshold) in enumerate(zip(columns, thresholds)):
+        value = group_conf_summary[column] > threshold
+        group_conf_summary['excluded'] = np.where(value == True, 1, 0)
+
+        p = sns.swarmplot(y=column,
                           x="task",
-                          hue="include",
                           data=group_conf_summary,
                           alpha=0.8,
                           s=10,
-                          color=palette,
-                          ax=axes[i]
-                          )
+                          hue='excluded',
+                          palette=palette,
+                          ax=axes[i])
 
-        p = sns.boxplot(y=value[0],
+        p = sns.boxplot(y=column,
                         x="task",
                         data=group_conf_summary,
                         showcaps=False,
                         boxprops={'facecolor': 'None'},
                         showfliers=False, ax=axes[i])
 
-        p.title.set_text(f"Threshold = {value[1]}")
-        p.axhline(value[1], ls='--', color="#fe6863")
+        p.title.set_text(f'Threshold = {threshold}')
+        p.axhline(threshold, ls='--', color="#fe6863")
         p.set(xlabel='')
-        p.set(ylabel=key)
+        p.set(ylabel=column)
         p.get_legend().set_visible(False)
         p.tick_params(axis='both', which='both', length=6, width=2.2)
-        i += 1
+
     fig.suptitle(f"Excluding high motion subjects", va="top")
+    fig.savefig(join(output_dir, f"motion_criterion_pipeline-{pipeline_name}.svg"), dpi=300)
 
     return fig
