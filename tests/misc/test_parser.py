541cd8a949cf937ade754285f4a8bf1348e8ef9f
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-06-22 11:18:13 +0200

    [TEST] fixed tests that failed after recent changes in pipeline json files structure

--- tests/misc/test_parser.py
+++ tests/misc/test_parser.py
@@ -3,22 +3,29 @@ from fmridenoise.__main__ import parse_pipelines
 import fmridenoise.pipelines as pipe
 from os.path import dirname, join
 from glob import glob
-class TestPipelinesParser(ut.TestCase):
 
+
+class TestPipelinesParser(ut.TestCase):
+    """
+    Test for parse_pipelines form __main__
+    """
     pipelines_dir = dirname(pipe.__file__)
     all_pipelines_valid = set(glob(join(pipelines_dir, "*.json")))
-    noicaaroma_pipelines_valid = set([p for p in all_pipelines_valid if 'ICA-AROMA' not in p])
+    noicaaroma_pipelines_valid = set([p for p in all_pipelines_valid if not pipe.is_IcaAROMA(pipe.load_pipeline_from_json(p))])
 
     def test_parse_pipelines_without_custom(self):
+        """Checks if parser accepts all pipelines from fmridenoise.pipeline"""
         paths = parse_pipelines("all")
         self.assertSetEqual(self.all_pipelines_valid, paths)
 
     def test_parse_pipelines_custom(self):
+        """Checks if parser accepts only custom pipeline"""
         custom = {join(dirname(__file__), "custom_pipeline.json")}
         paths = parse_pipelines(custom)
         self.assertSetEqual(custom, paths)
 
     def test_parse_pipelines_mix(self):
+        """Checks if parser accepts custom pipeline mixed with fmridenise.pipeline"""
         addition = join(dirname(__file__), "custom_pipeline.json")
         custom = self.all_pipelines_valid.copy()
         custom.add(addition)
@@ -26,6 +33,7 @@ class TestPipelinesParser(ut.TestCase):
         self.assertSetEqual(paths, custom)
 
     def test_parse_pipelines_known_pipeline(self):
+        """Checks if parser accepts pipeline from fmridenoise selected by name"""
         selected = "pipeline-24HMP_8Phys_SpikeReg_4GS"
         selected_path = {(join(self.pipelines_dir, selected) + ".json")}
         paths = parse_pipelines([selected]) # __main__ always return list of paths/selected pipelines names
