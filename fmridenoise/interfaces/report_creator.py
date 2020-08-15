d54265f42b5179e98a6a4729606b1dc06ef9b116
kbonna <kongokou@gmail.com>
2020-08-15 13:04:23 +0200

    changed input specification for ReportCreator interface

--- fmridenoise/interfaces/report_creator.py
+++ fmridenoise/interfaces/report_creator.py
@@ -9,28 +9,60 @@ class ReportCreatorInputSpec(BaseInterfaceInputSpec):
     
     output_dir = Directory(exists=True)
 
+    tasks_aggregated = List(Str(), mandaory=True)
+    sessions_aggregated = List(Str(), mandatory=False)
+    
+    output_dir = Directory(exists=True)
+
     # excluded_subjects = List(Str(), value=()) # TODO: This mayby another input field later. 
-    plot_pipelines_edges_density = List(File(
+    
+    # Aggregated over pipelines
+    plot_all_pipelines_edges_density = List(File(
         exists=True,
-        desc="Density of edge weights (all subjects)"
+        desc="Density of edge weights (all pipelines) for all subjects"
     ))
 
-    plot_pipelines_edges_density_no_high_motion = List(File(
+    plot_all_pipelines_edges_density_no_high_motion = List(File(
         exist=True,
-        desc="Density of edge weights (no high motion)"
+        desc="Density of edge weights (all pipelines) without high motion subjects"
     ))
 
-    plot_pipelines_fc_fd_pearson = List(File(
-        exist=True
+    plot_all_pipelines_fc_fd_pearson_info = List(File(
+        exist=True,
+        desc="Barplot and violinplot showing percent of significant fc-fd correlations and distribution of Pearson's r values for all subjects"
     ))
 
-    plot_pipelines_fc_fd_uncorr = List(File(
-        exist=True
+    plot_all_pipelines_fc_fd_pearson_info_no_high_motion = List(File(
+        exist=True,
+        desc="Barplot and violinplot showing percent of significant fc-fd correlations and distribution of Pearson's r values without high motion subjects"
     ))
 
-    plot_pipelines_distance_dependence = List(File(
-        exist=True
+    plot_all_pipelines_distance_dependence = List(File(
+        exist=True,
+        desc="Barplot showing mean Spearman's rho between fd-fc correlation and Euclidean distance between ROIs for all subject"
     ))
+
+    plot_all_pipelines_distance_dependence_no_high_motion = List(File(
+        exist=True,
+        desc="Barplot showing mean Spearman's rho between fd-fc correlation and Euclidean distance between ROIs without high motion subjects"
+    ))
+
+    plot_all_pipelines_tdof_loss = List(File(
+        exists=True,
+        desc="..."
+    ))
+
+    # For single pipeline
+    plot_pipeline_fc_fd_pearson_matrix = List(File(
+        exist=True,
+        desc="Matrix showing correlation between connection strength and motion for all subjects"
+    ))
+
+    plot_pipeline_fc_fd_pearson_matrix_no_high_motion = List(File(
+        exist=True,
+        desc="Matrix showing correlation between connection strength and motion without high motion subjects"
+    ))
+
 
 
 class ReportCreator(SimpleInterface):
