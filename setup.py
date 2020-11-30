925168e5ae4fd50f38fde505b5b0e5a817b68460
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-11-30 16:56:23 +0100

    versioneer added to project for better project version menagement

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
 import re
 from os.path import join, dirname
-dir_path = dirname(__file__)
+import versioneer
+
+__dir_path = dirname(__file__)
 
 
 def altered_long_description() -> str:
@@ -10,7 +12,7 @@ def altered_long_description() -> str:
     """
     git_problem_image = r'[<img src="https://github.com/compneuro-ncu/fmridenoise/blob/master/docs/fmridenoise_problem.png?raw=true">](https://github.com/compneuro-ncu/fmridenoise/blob/master/docs/fmridenoise_problem.png)'
     git_solution_image = r'[<img src="https://github.com/compneuro-ncu/fmridenoise/blob/master/docs/fmridenoise_solution.png?raw=true">](https://github.com/compneuro-ncu/fmridenoise/blob/master/docs/fmridenoise_solution.png)'
-    with open(join(dir_path, "README.md"), "r") as fh:
+    with open(join(__dir_path, "README.md"), "r") as fh:
         long_description = fh.read()
     problem_image = re.search(r'\!\[Problem image\].*?\n', long_description)
     if problem_image is None:
@@ -25,29 +27,31 @@ def altered_long_description() -> str:
     return long_description
 
 
-with open(join(dir_path, "requirements.txt"), 'r') as fh:
-    requirements = [line.strip() for line in fh]
+if __name__ == '__main__':
+    with open(join(__dir_path, "requirements.txt"), 'r') as fh:
+        requirements = [line.strip() for line in fh]
 
     setuptools.setup(
-    name="fmridenoise",
-    version="0.2.0.dev10",
-    author="Karolina Finc, Mateusz Chojnowski, Kamil Bonna",
-    author_email="karolinafinc@gmail.com, mateus.chojnowski@gmail.com, kongokou@gmail.com",
-    description="fMRIDenoise - automated denoising, denoising strategies comparison, and functional "
-                "connectivity data quality control.",
-    long_description=altered_long_description(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/nbraingroup/fmridenoise",
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        "Programming Language :: Python :: 3.6",
-    ],
-    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "*tests*",
-                                               '*build_tests*']),
-    install_requires=requirements,
-    license="License :: OSI Approved :: Apache Software License",
-    include_package_data=True,
-    scripts=[join(dir_path, 'fmridenoise', 'scripts', 'fmridenoise')]
-)
+        name="fmridenoise",
+        version=versioneer.get_version(),
+        author="Karolina Finc, Mateusz Chojnowski, Kamil Bonna",
+        author_email="karolinafinc@gmail.com, mateus.chojnowski@gmail.com, kongokou@gmail.com",
+        description="fMRIDenoise - automated denoising, denoising strategies comparison, and functional "
+                    "connectivity data quality control.",
+        long_description=altered_long_description(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/nbraingroup/fmridenoise",
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            "Programming Language :: Python :: 3.6",
+        ],
+        packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "*tests*",
+                                                   '*build_tests*']),
+        install_requires=requirements,
+        license="License :: OSI Approved :: Apache Software License",
+        include_package_data=True,
+        cmdclass=versioneer.get_cmdclass(),
+        scripts=[join(__dir_path, 'fmridenoise', 'scripts', 'fmridenoise')]
+    )
