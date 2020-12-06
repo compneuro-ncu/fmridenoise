7ca509f9783a7c6127a7f9d186c74dc576a31545
Siegfried Wagner <mateus.chojnowski@gmail.com>
2020-12-06 08:54:15 +0100

    changes need to fix travis (#69)
    * changes need to fix travis badge in readme
    
    * upgraded minimum required version of python, added pip upgrade to travis

--- fmridenoise/utils/error_data.py
+++ fmridenoise/utils/error_data.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from dataclasses import dataclass, asdict
 from fmridenoise.utils.entities import build_path
 
@@ -11,11 +10,11 @@ class ErrorData:
     message: str
 
     @classmethod
-    def warning(cls, entities: dict, source: object, message: str) -> ErrorData:
+    def warning(cls, entities: dict, source: object, message: str) -> "ErrorData":
         return ErrorData(critical=False, entities=entities, source_name=source.__class__.__name__, message=message)
 
     @classmethod
-    def error(cls, entities: dict, source: object, message: str) -> ErrorData:
+    def error(cls, entities: dict, source: object, message: str) -> "ErrorData":
         return ErrorData(critical=True, entities=entities, source_name=source.__class__.__name__, message=message)
 
     def asdict(self):
