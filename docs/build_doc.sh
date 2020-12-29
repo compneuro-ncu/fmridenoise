8b9d64f1f82b7b497cf765ecf622ce84543a15e9
Siegfried Wagner <mateus.chojnowski@gmail.com>
2020-12-29 20:55:45 +0100

    readthe docs improvements (#85)

--- docs/build_doc.sh
+++ docs/build_doc.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit 1
-PYTHONPATH=$PYTHONPATH:$(dirname "$PWD")
-export PYTHONPATH
 sphinx-apidoc -P -o ./source/apidoc --no-toc ../fmridenoise
 make html
