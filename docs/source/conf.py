a92c2154496bfca28406d65b12147f9aa3ce0758
SiegfriedWagner <mateus.chojnowski@gmail.com>
2020-12-29 20:47:02 +0100

    readthe docs improvements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,11 +11,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-#
-import sphinx_rtd_theme
+import pathlib as p
+sys.path.append(str(p.Path(__file__).parent.parent.parent))
 # -- Project information -----------------------------------------------------
 
 project = 'fmridenoise'
